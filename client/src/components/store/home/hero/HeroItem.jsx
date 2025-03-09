34790fbcf057faa64d8b20e53c839db1e6a0812e
Elliot Sand-Warge <elliotsandmail@gmail.com>
2025-03-09 19:24:27 +0100

    fix how bg image inserted into component

--- client/src/components/store/home/hero/HeroItem.jsx
+++ client/src/components/store/home/hero/HeroItem.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router";
 
 export default function HeroItem({ img, text, buttonlabel, href, invisibleText }) {
  return (
-        <div className={`bg-[url(${img})] bg-cover bg-center h-full w-screen max-w-[1024px] text-white flex justify-center items-end py-golden-2xl select-none`}>
+        <div className={`bg-cover bg-center h-full w-screen max-w-[1024px] text-white flex justify-center items-end py-golden-2xl select-none`} style={{backgroundImage: `url(${img}`}}>
             <div className="flex flex-col items-center gap-golden-xl px-golden-lg">
                 <h2 className={`text-lg text-center font-bold tracking-wider leading-9 drop-shadow-xl ${invisibleText && "invisible"}`}>{text}</h2>
                 <Link to={href} className="bg-white text-black font-bold w-32 text-center py-golden-md rounded-md block tracking-wide shadow-lg shadow-black hover:outline-2 outline-primary">{buttonlabel}</Link>
