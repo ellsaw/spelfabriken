3a8de06e3a9946148449f0a8dc5e5a3f274f3ec2
Elliot Sand-Warge <elliotsandmail@gmail.com>
2025-03-10 20:35:21 +0100

    add sticky header

--- client/src/components/admin/Header/Header.jsx
+++ client/src/components/admin/Header/Header.jsx
@@ -2,7 +2,7 @@ import NavLink from "./NavLink";
 
 export default function Header() {
  return (
-    <header className="bg-pure-white mb-golden-xl text-primary px-golden-lg py-golden-md flex justify-center shadow-sm">
+    <header className="bg-pure-white mb-golden-xl text-primary px-golden-lg py-golden-md flex justify-center shadow-sm sticky top-0">
         <div className="justify-between flex items-center max-w-[1024px] w-full">
             <h1 className="text-md font-semibold">LOGO Dashboard</h1>
             <nav className="flex gap-golden-md">
