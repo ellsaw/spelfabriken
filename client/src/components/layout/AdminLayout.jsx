d61672d7d73ed8f35e7a8f99f120b18da57def64
Elliot Sand-Warge <elliotsandmail@gmail.com>
2025-03-11 13:55:50 +0100

    remove off white coloring

--- client/src/components/layout/AdminLayout.jsx
+++ client/src/components/layout/AdminLayout.jsx
@@ -1,15 +1,15 @@
 import { Outlet } from "react-router";
-import Header from "../admin/Header/Header.jsx";
+import Header from "../admin/header/Header.jsx";
 
 export default function PublicLayout() {
 
  return (
     <>
     <title>Dashboard</title>
-    <div className="bg-white font-kanit min-h-[100dvh] flex flex-col text-black">
+    <div className="bg-neutral-300 font-kanit min-h-[100dvh] flex flex-col text-black">
     <Header/>
     <div className="flex justify-center flex-1">
-    <main className="flex justify-center max-w-[640px] bg-pure-white flex-1 shadow-sm">
+    <main className="flex justify-center max-w-[640px] bg-white flex-1 shadow-sm">
         <Outlet/>
     </main>
     </div>
