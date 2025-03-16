a71e3bd4443ea949f02dcf931869696de2f7475f
Elliot Sand-Warge <elliotsandmail@gmail.com>
2025-03-16 13:03:03 +0100

    fix bug that prevented scrolling on the body when sidebar closed, and changed to useRef in sidebar

--- client/src/components/store/header/sidebar/Sidebar.jsx
+++ client/src/components/store/header/sidebar/Sidebar.jsx
@@ -1,28 +1,26 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef} from "react";
 import SidebarContent from "./SidebarContent.jsx";
 
 export default function Sidebar({ toggleSidebar }) {
+  const sidebarRef = useRef(null);
+  const overlayRef = useRef(null)
 
     const handleSidebar = {
         initialize(){
-            const overlay = document.getElementById("overlay");
-            const sidebar = document.getElementById("sidebar");
             const body = document.querySelector("body");
             
             setTimeout(() => {
-                overlay.classList.remove("opacity-0")
-                sidebar.classList.remove("scale-x-0")
-                body.classList.add("overflow-hidden")
+              overlayRef.current.classList.remove("opacity-0")
+              sidebarRef.current.classList.remove("scale-x-0")
+              body.classList.add("overflow-hidden")
             }, 1);
         },
         close(){
-            const overlay = document.getElementById("overlay");
-            const sidebar = document.getElementById("sidebar");
             const body = document.querySelector("body");
 
-            overlay.classList.add("opacity-0")
-            sidebar.classList.add("scale-x-0")
-            body.classList.add("overflow-hidden")
+            overlayRef.current.classList.add("opacity-0")
+            sidebarRef.current.classList.add("scale-x-0")
+            body.classList.remove("overflow-hidden")
 
             setTimeout(() => {
                 toggleSidebar()
@@ -36,10 +34,10 @@ export default function Sidebar({ toggleSidebar }) {
 
   return (
     <>
-      <aside id="sidebar" className="h-[100dvh] bg-white w-[90vw] max-w-[350px] fixed right-0 top-0 z-50 scale-x-0 origin-right transition-transform duration-150 overflow-y-auto">
+      <aside ref={sidebarRef} className="h-[100dvh] bg-white w-[90vw] max-w-[350px] fixed right-0 top-0 z-50 scale-x-0 origin-right transition-transform duration-150 overflow-y-auto">
         <SidebarContent close={handleSidebar.close}/>
       </aside>
-      <div id="overlay" className="fixed top-0 left-0 size-full bg-neutral-950/20 backdrop-blur-sm z-40 opacity-0 transition-opacity duration-150" onClick={handleSidebar.close}></div>
+      <div ref={overlayRef} className="fixed top-0 left-0 size-full bg-neutral-950/20 backdrop-blur-sm z-40 opacity-0 transition-opacity duration-150" onClick={handleSidebar.close}></div>
     </>
   );
 }
